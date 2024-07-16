8a18011d5962a7cc8218d03abe2509cd8746410e
Chris Agnew <chris@chris-agnew.com>
2024-07-16 02:10:14 -0400

    refactor(api): refactor algolia code to be in the algolia util

--- src/utils/algolia.ts
+++ src/utils/algolia.ts
@@ -1,24 +1,28 @@
 import algoliasearch from "algoliasearch";
 import { BuilderPage } from "@/utils/builder";
 
-const algoliaAppId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID;
-const algoliaApiKey = process.env.NEXT_PUBLIC_ALGOLIA_API_KEY;
-const algoliaIndexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
-
-const algoliaClient = algoliasearch(algoliaAppId!, algoliaApiKey!);
-const index = algoliaClient.initIndex(algoliaIndexName!);
+const client = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
+  process.env.NEXT_PUBLIC_ALGOLIA_API_KEY!
+);
+const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME!);
 
 export const indexPages = async (pages: BuilderPage[]): Promise<void> => {
-  const objects = pages.map((page) => ({
-    objectID: page.id,
-    title: page.data.title,
-    description: page.data.description,
-    url: page.data.url,
-    content: page.data.blocks
-      .map((block: any) => block.component?.options?.text)
-      .join(" "),
-  }));
+  try {
+    const objects = pages.map((page) => ({
+      objectID: page.id,
+      title: page.data.title,
+      description: page.data.description,
+      url: page.data.url,
+      content: page.data.blocks
+        .map((block: any) => block.component?.options?.text)
+        .join(" "),
+    }));
 
-  await index.saveObjects(objects);
-  console.log("Pages indexed to Algolia");
+    await index.saveObjects(objects);
+    console.log("Pages indexed to Algolia");
+  } catch (error: any) {
+    console.error("Error indexing content to Algolia:", error);
+    throw new Error("Indexing to Algolia failed");
+  }
 };
