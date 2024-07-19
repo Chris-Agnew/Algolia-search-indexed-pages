8e810cc4b41ff988eb3d3eb2cf64c2d0a3a2e3c8
Chris Agnew <hello@chrisagnew.codes>
2024-07-19 08:05:53 -0400

    chore(env): update gitignore and add example for cypress env for testing

---
+++ cypress.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  env: {
+    NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+    NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY:
+      process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+    NEXT_PUBLIC_ALGOLIA_API_KEY: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
+    NEXT_PUBLIC_ALGOLIA_INDEX_NAME: process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME,
+  },
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+    indexHtmlFile: "cypress/support/component-index.html",
+    supportFile: "cypress/support/component.ts",
+  },
+  e2e: {
+    baseUrl: "https://alipes-dev-assessment.vercel.app",
+    supportFile: "cypress/support/e2e.ts",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: false,
+});
